dff9cf68d44436f1c83fe11e9f04b83b39a3f71d
naufalfarel <naufalfaerel@gmail.com>
2025-10-02 23:04:51 +0700

    feat: memperbaiki routing app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Login from "./components/Login";
 import SignIn from "./components/SignIn";
