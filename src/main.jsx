23d2411fdd6c3120e975a03ff396eb08a1cf78b6
naufalfarel <naufalfaerel@gmail.com>
2025-10-02 15:19:57 +0700

    fix: perbaiki routing navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import './index.css'; // Mengimpor gaya atau CSS
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Mengimpor modul React untuk rendering ke DOM
-import App from './App'; // Mengimpor komponen App, komponen utama aplikasi
+import "./index.css"; // Mengimpor gaya atau CSS
+import React from "react";
+import ReactDOM from "react-dom/client"; // Mengimpor modul React untuk rendering ke DOM
+import App from "./App"; // Mengimpor komponen App, komponen utama aplikasi
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- 
-      <App />
-   
+    <App />
   </React.StrictMode>
 );
